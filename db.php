ed886a136743164744a81727ee74af545a661ba8
VedicXcel Innovations <drona.gaming@abhishektarafdar.in>
2025-04-03 16:23:30 +0530

    u

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = 'localhost';
-    $username ='root';
-    $password ='';
-    $database_name ='ngo';
-
-    $con = mysqli_connect($server,$username,$password,$database_name);
-?>
